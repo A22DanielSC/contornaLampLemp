a670e83df37e6761c95e734b5a4f82335d5ec626
A22DanielSC <116753189+A22DanielSC@users.noreply.github.com>
2024-03-05 14:32:27 +0100

    Delete contenedorExercicio directory

--- contenedorExercicio/database/backup/backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose exec basedatos bash -c "mysqldump -u a22danielsc -p prestashowdb" > ./database/backup/backup.sql
--- contenedorExercicio/database/backup/restore.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose exec basedatos bash -c "mysql -u a22danielsc -pabc123. prestashowdb" < ./database/backup/backup.sql
